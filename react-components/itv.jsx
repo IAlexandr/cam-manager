40161306ab08e15b9877aa4065c6312edede39e7
Alexandr Ivanov <aivanov@informatica.ru>
2015-02-01 15:31:18 +0300

    правка отступов

--- react-components/itv.jsx
+++ react-components/itv.jsx
@@ -82,84 +82,91 @@ var CamMonitor = React.createClass({
             case MODE_CONNECT:
             case MODE_SHOW:
                 mainContent = (
-                <object
-                ref = "camMon"
-                classID = { "clsid:" + ACTIVEX_ITV_CLASS_ID }
-                />
-            );
+                    <object
+                        ref = "camMon"
+                        classID = { "clsid:" + ACTIVEX_ITV_CLASS_ID }
+                    />
+                );
                 break;
             case MODE_ERROR:
                 mainContent = (
-                <div>
-                <p><b>Ошибка</b></p>
-            <p>{ this.state.errorMessage }</p>
-        </div>
+                    <div>
+                        <p>
+                            <b>Ошибка</b>
+                        </p>
+                        <p>{ this.state.errorMessage }</p>
+                    </div>
+                );
+                break;
+            default:
+                throw new Error("Unknown mode.");
+        }
+
+        return (
+            <div>
+                <b>server ip: { this.props.serverIp }</b>
+                <br />
+                <b>cam id: { this.props.camId }</b>
+                <br />
+                <b>mode: { this.state.mode }</b>
+                <br />
+                <b>error msg: { this.state.errorMessage }</b>
+                <br />
+                <div>{ mainContent }</div>
+            </div>
         );
-        break;
-        default: throw new Error("Unknown mode.");
-    }
+    },
 
-    return (
-    <div>
-    <b>server ip: { this.props.serverIp }</b><br />
-<b>cam id: { this.props.camId }</b><br />
-<b>mode: { this.state.mode }</b><br />
-<b>error msg: { this.state.errorMessage }</b><br />
-{ mainContent }
-</div>
-);
-},
+    startConnect: function () {
+        var _this = this;
+        this.toHandle = setTimeout(function () {
+            clearTimeout(_this.tickHandle);
+            clearTimeout(_this.checkConnectTimeoutHandle);
+            _this.setState({
+                mode: MODE_ERROR,
+                errorMessage: 'Connection timeout'
+            });
+        }, CONNECT_TIMEOUT);
 
-startConnect: function () {
-    var _this = this;
-    this.toHandle = setTimeout(function () {
-        clearTimeout(_this.tickHandle);
-        clearTimeout(_this.checkConnectTimeoutHandle);
-        _this.setState({
-            mode: MODE_ERROR,
-            errorMessage: 'Connection timeout'
-        });
-    }, CONNECT_TIMEOUT);
+        this.tryConnect();
+        this.checkConnect();
+    },
 
-    this.tryConnect();
-    this.checkConnect();
-},
+    tryConnect: function () {
+        var cmNode = this.refs.camMon.getDOMNode();
 
-tryConnect: function () {
-    var cmNode = this.refs.camMon.getDOMNode();
+        cmNode.object.Connect(this.props.serverIp, "", "", "", 0);
+        this.tickHandle = setTimeout(this.tryConnect, CONNECT_TICK_INTERVAL);
+    },
 
-    cmNode.object.Connect(this.props.serverIp, "", "", "", 0);
-    this.tickHandle = setTimeout(this.tryConnect, CONNECT_TICK_INTERVAL);
-},
+    checkConnect: function () {
+        var cmNode = this.refs.camMon.getDOMNode();
+        if (cmNode.object.isConnected() === 1) {
+            clearTimeout(this.toHandle);
+            clearTimeout(this.tickHandle);
+            this.setState({
+                mode: MODE_CONNECT
+            });
+            this.startShowCamera();
+        } else {
+            this.checkConnectTimeoutHandle = setTimeout(this.checkConnect, CHECKCONNECT_INTERVAL);
+        }
+    },
 
-checkConnect: function () {
-    var cmNode = this.refs.camMon.getDOMNode();
-    if (cmNode.object.isConnected() === 1) {
-        clearTimeout(this.toHandle);
-        clearTimeout(this.tickHandle);
+    startShowCamera: function () {
+        this.cameraShowCounter = CAMERA_SHOW_TIMES;
+        this.tryShowCamera();
         this.setState({
-            mode: MODE_CONNECT
+            mode: MODE_SHOW
         });
-        this.startShowCamera();
-    } else {
-        this.checkConnectTimeoutHandle = setTimeout(this.checkConnect, CHECKCONNECT_INTERVAL);
-    }
-},
+    },
 
-startShowCamera: function () {
-    this.cameraShowCounter = CAMERA_SHOW_TIMES;
-    this.tryShowCamera();
-    this.setState({
-        mode: MODE_SHOW
-    });
-},
-
-tryShowCamera: function () {
-    var cmNode = this.refs.camMon.getDOMNode();
-    cmNode.object.ShowCam(this.props.camId, 0, 1);
-    if (--this.cameraShowCounter > 0) {
-        this.camToHandle = setTimeout(this.tryShowCamera, CAMERA_SHOW_INTERVAL);
+    tryShowCamera: function () {
+        var cmNode = this.refs.camMon.getDOMNode();
+        cmNode.object.ShowCam(this.props.camId, 0, 1);
+        if (--this.cameraShowCounter > 0) {
+            this.camToHandle = setTimeout(this.tryShowCamera, CAMERA_SHOW_INTERVAL);
+        }
     }
-}
 
 });
